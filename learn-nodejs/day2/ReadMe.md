3023c242efbd2bdb360411b232e69db6a219d200
Gaurav Madaan <gaurav.madaan@techrbm.com>
2022-07-20 12:24:49 +0530

    Added controllers models

---
+++ learn-nodejs/day2/ReadMe.md
@@ -0,0 +1,22 @@
+# Day 2 of Training in Nodejs
+
+## Using MongoDB cloud
+- Create account on cloud.mongodb.com
+- Create a shared cluster
+- Select option Connect using application 
+- Copy connection string
+
+## Understanding Environment File 
+- .env file for configurations
+- dotenv package
+- accessing environment variables using process.env.<variable>
+
+## Implementing API in application
+- Creating schema for MongoDB
+- Create password encyption using bcryptjs
+- Implementing Validation
+- Create model
+- Create controller
+- Create route
+- Register routes in app.js
+
---
+++ learn-nodejs/day2/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter name"],
+    maxlength: 50,
+    minlength: 5,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter email"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please enter valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter password"],
+    minlength: 8,
+  },
+});
+
+UserSchema.pre("save", async function() {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  
+});
+
+module.exports = mongoose.model('User', UserSchema);
